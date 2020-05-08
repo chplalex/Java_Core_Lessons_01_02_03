e581c6b9b41e9d8d650e0fbd27764bcf399c2a2c
Alexander Chepel <chepel.alexander@gmail.com>
2020-05-08 11:52:23 +0300

    Fix & add
    Fix minor bag in lesson 0102 and add manifest for project.

--- src/lesson0102/MainApp0102.java
+++ src/lesson0102/MainApp0102.java
@@ -59,7 +59,7 @@ public class MainApp0102 {
         int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         System.out.println(Arrays.toString(arr));
 
-        for (var i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             if (arr[i] < 6) {
                 arr[i] = arr[i] * 2;
             }

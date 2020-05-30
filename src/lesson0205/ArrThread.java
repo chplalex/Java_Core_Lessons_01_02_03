04e53abeaea2c9c29f7724ea15adbca7e8143194
chplalex <chepel.alexander@gmail.com>
2020-05-30 10:01:48 +0300

    Lesson 0205 is done and ready for checking.

---
+++ src/lesson0205/ArrThread.java
@@ -0,0 +1,32 @@
+package lesson0205;
+
+public class ArrThread extends Thread {
+    private float[] arr;
+    private int diff;
+
+    public ArrThread(int size, int diff) {
+        this.diff = diff;
+        arr = new float[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1;
+        }
+    }
+
+    public float[] getArr() {
+        return arr;
+    }
+
+    public void addArr(float[] arr2) {
+        float[] arr1 = arr;
+        arr = new float[arr1.length + arr2.length];
+        System.arraycopy(arr1, 0, arr, 0, arr1.length);
+        System.arraycopy(arr2, 0, arr, arr1.length, arr2.length);
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + (i + diff) / 5) * Math.cos(0.2f + (i + diff) / 5) * Math.cos(0.4f + (i + diff) / 2));
+        }
+    }
+}
--- src/lesson0205/MainApp0205.java
+++ src/lesson0205/MainApp0205.java
@@ -4,61 +4,36 @@ public class MainApp0205 {
     private static int SIZE = 10000000;
     private static int HALF = SIZE / 2;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+        System.out.println("START");
         method1();
         method2();
+        System.out.println("END");
 
     }
 
-    public static void initArr(float[] arr) {
-        for (float f : arr) {
-            f = 1;
-        }
-    }
-
-    public static void calcArr(float[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        }
-    }
-
-    public static void method1() {
-        float[] arr = new float[SIZE];
-        initArr(arr);
+    public static void method1() throws InterruptedException {
+        ArrThread arrThread = new ArrThread(SIZE, 0);
         long a = System.currentTimeMillis();
-        calcArr(arr);
+        arrThread.start();
+        arrThread.join();
         long b = System.currentTimeMillis();
-        System.out.printf("Method1. Вычисление в 1 потоке.         Время выполнения = %d миллисекунд.\n", (b - a));
+        System.out.printf("Method1. Вычисление в 1 потоке.  Время выполнения = %d миллисекунд.\n", (b - a));
     }
 
-    public static void method2() {
-        long a, b;
-        float[] arr = new float[SIZE];
+    public static void method2() throws InterruptedException {
+        ArrThread arrThread1 = new ArrThread(HALF, 0);
+        ArrThread arrThread2 = new ArrThread(HALF, HALF);
 
-        initArr(arr);
-
-        float[] arr1 = new float[HALF];
-        float[] arr2 = new float[HALF];
-
-        a = System.currentTimeMillis();
-        System.arraycopy(arr, 0, arr1, 0, HALF);
-        System.arraycopy(arr, HALF, arr2, 0, HALF);
-        b = System.currentTimeMillis();
-        System.out.printf("Method2. Копирование arr -> arr1, arr2. Время выполнения = %d миллисекунд.\n", b - a);
-
-        a = System.currentTimeMillis();
-        new Thread(() -> calcArr(arr1)).start();
-        new Thread(() -> calcArr(arr2)).start();
-        b = System.currentTimeMillis();
-        System.out.printf("Method2. Вычисление в 2 потоках.        Время выполнения = %d миллисекунд.\n", b - a);
-
-        a = System.currentTimeMillis();
-        System.arraycopy(arr1, 0, arr, 0, HALF);
-        System.arraycopy(arr2, 0, arr, HALF, HALF);
-        System.out.printf("Method2. Копирование arr1, arr2 -> arr. Время выполнения = %d миллисекунд.\n", b - a);
-
+        long a = System.currentTimeMillis();
+        arrThread1.start();
+        arrThread2.start();
+        arrThread1.join();
+        arrThread2.join();
+        arrThread1.addArr(arrThread2.getArr());
+        long b = System.currentTimeMillis();
+        System.out.printf("Method2. Вычисление в 2 потоках. Время выполнения = %d миллисекунд.\n", b - a);
     }
-
 
 }

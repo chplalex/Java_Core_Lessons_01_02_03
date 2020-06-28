1138f158d46aab4e6634a60c57aa3cb3d9590728
Alexander Chepel <chepel.alexander@gmail.com>
2020-06-28 16:18:58 +0300

    Initial commit

---
+++ src/lesson0304/MainApp0304.java
@@ -0,0 +1,11 @@
+package lesson0304;
+
+public class MainApp0304 {
+    public static void main(String[] args) {
+        MainApp0304 ma = new MainApp0304();
+        ma.doTask();
+    }
+
+    private void doTask() {
+    }
+}
